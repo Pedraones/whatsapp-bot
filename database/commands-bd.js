6f83f003de0972b974fb4be37cbf1e21879ee6d0
Pedro Augusto <perobr2018@gmail.com>
2026-04-18 14:38:44 -0300

    dev: feat - Consulta por nomes

--- database/commands-bd.js
+++ database/commands-bd.js
@@ -11,6 +11,23 @@ async function getWithoutParameters(){
     return result.rows;
 }
 
+async function getWithParameters(nome){
+    if(nome){
+        const value = [nome.toLowerCase()];
+        await client.client.connect();
+        
+        const result = await client.client.query('SELECT (idpedido, clientepedido, comandapedido, valorpedido) FROM pedidos WHERE clientepedido = $1', value);
+        await client.client.end();
+        
+        console.log(result.rows);
+        return result.rows;
+    }
+    else{
+        console.log("passe um nome");
+    }
+}
+
 module.exports = {
-    getWithoutParameters:getWithoutParameters
+    getWithoutParameters:getWithoutParameters,
+    getWithParameters: getWithParameters
 }
