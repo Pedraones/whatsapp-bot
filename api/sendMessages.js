13f35d8a763a0d88222b66b889b8eb4ed4762859
Pedro Augusto <perobr2018@gmail.com>
2026-04-19 15:38:08 -0300

    dev: feat - Enviando instruções de como adicionar um novo pedido

--- api/sendMessages.js
+++ api/sendMessages.js
@@ -22,6 +22,32 @@ async function sendListCommands(){
   .then(json => console.log(json)).catch(err => console.error(err));
 }
 
+async function sendStructAddOrder(){
+  var message = "Para adicionar um novo pedido siga o seguinte formato: \n" + 
+                "Nome: \n" + 
+                "Pedido: \n" + 
+                "Valor: \n" +
+                "Mais instrucoes: Mantenha os campos sinalizados anteriormente, apenas complete com as informacoes necessarias; Em 'Valor' nao insira 'R$', apenas coloque o valor, separando inteiros de centavos com um ponto(.); Em 'Finalizado', 'nao' deve ser sem ~";
+
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      authorization: process.env.TOKEN_API
+    },
+    body: JSON.stringify({
+      typing_time: 0,
+      to: '5515998282773@s.whatsapp.net',
+      body: message
+    })
+  };
+
+  fetch(url, options).then(res => res.json())
+  .then(json => console.log(json)).catch(err => console.error(err));
+}
+
 module.exports = {
-    sendListCommands: sendListCommands
+  sendListCommands: sendListCommands,
+  sendStructAddOrder: sendStructAddOrder
 }
