0276e3103f1dada4f435c6ec122672743de229b3
Pedro Augusto <perobr2018@gmail.com>
2026-04-19 15:44:02 -0300

    dev: fix - Chamada das instruçoes de ver pedidos especificos no lugar correto

--- server/verifyMessages.js
+++ server/verifyMessages.js
@@ -14,10 +14,10 @@ async function getMessages(messages){
                     
                     case "#INSTRUCAO VER PEDIDOS#":
                         console.log("instrucao ver pedidos");
-                        send.sendStructGetOrderSpecified()
                         break;
-        
+                        
                     case "#INSTRUCAO VER PEDIDO ESPECIFICO#":
+                        send.sendStructGetOrderSpecified()
                         console.log("instrucao ver pedido especifico");
                         break;
                     
