4ff536cf53fc30012ed1e7468cfe5f9f5e5e10b3
Pedro Augusto <perobr2018@gmail.com>
2026-04-19 15:30:23 -0300

    dev: feat - Verificando as mensagens apenas do contato correto

--- server/verifyMessages.js
+++ server/verifyMessages.js
@@ -1,26 +1,33 @@
+const send = require('../api/sendMessages');
+
 async function getMessages(messages){
+    const chatCorrect = "5515996180975@s.whatsapp.net"
+    
     if(messages){
         messages.forEach(element => {
-            switch (element.text.body){
-                case "#LISTAR COMANDOS#":
-                    console.log("listar pedidos");
+            if(element.text && element.chat_id == chatCorrect){    
+                switch (element.text.body){
+                    case "#LISTAR COMANDOS#":
+                        console.log("listar comandos")
+                        send.sendListCommands();
+                        break;
+                    
+                    case "#VER PEDIDOS#":
+                        console.log("ver pedidos");
+                        break;
+        
+                    case "#VER PEDIDO ESPECIFICO#":
+                        console.log("ver pedido especifico");
+                        break;
+                    
+                    case "#ADICIONAR PEDIDOS#":
+                    console.log("adicionar pedidos");
                     break;
-                
-                case "#VER PEDIDOS#":
-                    console.log("ver pedidos");
+                    
+                    case "#ATUALIZAR PEDIDO#":
+                    console.log("atualizar pedidos");
                     break;
-    
-                case "#VER PEDIDO ESPECIFICO#":
-                    console.log("ver pedido especifico");
-                    break;
-                
-                case "#ADICIONAR PEDIDOS#":
-                console.log("adicionar pedidos");
-                break;
-                
-                case "#ATUALIZAR PEDIDO#":
-                console.log("atualizar pedidos");
-                break;
+                }
             }
         });
     }
