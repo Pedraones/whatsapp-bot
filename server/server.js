fc32c5ae0bedff8ffd204632075d07e5983cf6dc
Pedro Augusto <perobr2018@gmail.com>
2026-05-08 16:14:13 -0300

    dev: feat - Verificando a origem das requisições

--- server/server.js
+++ server/server.js
@@ -1,22 +1,64 @@
 const express = require('express');
 const app = express();
-const verify = require('./verifyMessages')
-const send = require('../api/sendMessages')
-const call = require('../database/callCommands')
-const commands = require('../database/commands-bd')
+const verify = require('./verifyMessages');
+const send = require('../api/sendMessages');
+const call = require('../database/callCommands');
+const commands = require('../database/commands-bd');
+const verifyReq = require('./verifyReq');
 
 app.use(express.json());
 
 app.get('/');
 
 app.post('/webhook', async (req, res) => {
+  const channelIdReq = await req.body.channel_id;
+  const rawHeadersReq = await req.rawHeaders;
+
+  const secure = verifyReq.verifyReq(channelIdReq, rawHeadersReq);
+
+  if(secure){
     const messages = await req.body.messages;
     const messageProcessed = await verify.getMessages(messages);
     if(messageProcessed){
       call.callComandBd(messageProcessed)
     }
+  }
 });
 
 app.listen(3000, () => {
   console.log('Server started');
 });
+
+/*
+ body: {
+    messages: [ [Object] ],
+    event: { type: 'messages', event: 'post' },
+    channel_id: 'PUNISH-86FWR'
+  },
+
+*/
+
+/*
+rawHeaders: [
+    'Host',
+    'unquarrelling-ibrahim-sanguinolent.ngrok-free.dev',
+    'Content-Length',
+    '230',
+    'Accept',
+    'application/json',
+    'Baggage',
+    'sentry-environment=prod,sentry-public_key=22217dc31eef5d21afad60404664acb9,sentry-trace_id=84161c37841145d58974dd2ceb8d2d90',
+    'Content-Type',
+    'application/json',
+    'Sentry-Trace',
+    '84161c37841145d58974dd2ceb8d2d90-a2fb0382648c7d70',
+    'X-Forwarded-For',
+    '157.180.57.166',
+    'X-Forwarded-Host',
+    'unquarrelling-ibrahim-sanguinolent.ngrok-free.dev',
+    'X-Forwarded-Proto',
+    'https',
+    'Accept-Encoding',
+    'gzip'
+  ],
+*/
---
+++ server/verifyReq.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+function verifyReq(channelIdParam, rawHeaders){
+    const channelIdOfParam = channelIdParam;
+    const channelId = process.env.CHANNEL_ID_BODY_REQ;
+    const baggage = process.env.BAGGAGE_REQ;
+    const xForwradedFor = process.env.X_FORWARDED_FOR;
+    const xForwradedHost = process.env.X_FORWARDED_HOST;
+
+    const fieldsRawHeaders = [rawHeaders[7], rawHeaders[13], rawHeaders[15]];
+
+    if(channelId == channelIdOfParam && 
+        fieldsRawHeaders[0] == baggage && 
+        fieldsRawHeaders[1] == xForwradedFor && 
+        fieldsRawHeaders[2] == xForwradedHost) return true;
+
+    return false;
+}
+
+module.exports = {
+    verifyReq: verifyReq
+}
