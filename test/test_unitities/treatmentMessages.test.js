68fcdfdaccfd679eea001c79d145aeb2c2a62722
Pedro Augusto <perobr2018@gmail.com>
2026-05-11 21:10:12 -0300

    dev: feat - Teste para verificação da mensagem tratada para atualizar o campo 'finalizado' de um pedido

--- test/test_unitities/treatmentMessages.test.js
+++ test/test_unitities/treatmentMessages.test.js
@@ -13,3 +13,15 @@ test("Teste para verificação do tratamento de mensagem para adicionar um pedid
 
     expect(["#ADICIONAR PEDIDO#", "2 broches pequenos", "20.5", "Beatriz"]).toEqual(messageTreated);
 });
+
+describe("Teste para verificação do tratamento de mensagem para atualizar um pedido", () => {
+    const newDatas = ["#ATUALIZAR PEDIDO#", 2];
+
+    test("Atualizar um pedido no campo finalizado", () => {
+        newDatas.push("finalizado");
+        newDatas.push("s");
+        const messageTreated = treatment.treatmentUpdateOrder(newDatas);
+
+        expect(messageTreated).toEqual(["#ATUALIZAR PEDIDO#", 2, "finalizado", true]);
+    })
+});
