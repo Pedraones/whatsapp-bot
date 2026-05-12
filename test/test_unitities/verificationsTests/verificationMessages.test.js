f1108bf26fbe249ab51ae924c20ee96dca19ffbc
Pedro Augusto <perobr2018@gmail.com>
2026-05-12 07:37:21 -0300

    dev: feat - Teste criado para verificar o contato que enviou mensagem

---
+++ test/test_unitities/verificationsTests/verificationMessages.test.js
@@ -0,0 +1,14 @@
+const verify = require('../../../verifications/verifyMessages');
+
+test("Teste de verificação do contato que enviou mensagem", async () => {
+    const message = [
+        {
+            chat_id: "5515998942168@s.whatsapp.net",
+            text: "Verificando o contato que enviou a mensagem",
+        }
+    ];
+
+    const res = verify.getMessages(message);
+
+    expect(res).toBeNull();
+});
