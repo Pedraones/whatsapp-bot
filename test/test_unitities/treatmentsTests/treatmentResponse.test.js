434b2e98e5a1f330f01dc3b295c4d8f5b93c09b4
Pedro Augusto <perobr2018@gmail.com>
2026-05-12 07:26:49 -0300

    dev: feat - Implementação de teste para verificação do tratamento da consulta no bd

--- test/test_unitities/treatmentsTests/treatmentResponse.test.js
+++ test/test_unitities/treatmentsTests/treatmentResponse.test.js
@@ -1,9 +1,10 @@
 const treatment = require('../../../treatments/treatmentResponse');
 
-test("Teste do tratamento de mensagens do banco de dados", () => {
-    const simulationResponse = {row: '(3, "pedro        ", "4 broches grandes     ", 20.4)'};
+test("Teste do tratamento de mensagens do banco decd  dados", () => {
+    const simulationResponse = 
+    {row: '(3,"pedro        ","4 broches grandes     ",20.4)'};
 
-    const responseTreated = treatment.treatmentResponseBD(simulationResponse);
+    const responseTreated = treatment.treatmentResponseBD(simulationResponse.row);
 
-    expect(responseTreated).toEqual(['3', 'pedro', '4 broches grandes', '20.4']);
+    expect(responseTreated).toEqual(['3','pedro', '4 broches grandes','20.4']);
 })
