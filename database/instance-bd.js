1b4f70835c8ac04070db7c2f14998a00d93caa5b
Pedro Augusto <perobr2018@gmail.com>
2026-05-13 21:41:44 -0300

    dev: refactor - Adaptando o módulo 'instance-bd' para que passe no seu respectivo teste

--- database/instance-bd.js
+++ database/instance-bd.js
@@ -1,14 +1,23 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-    user: process.env.USERNAME_BD,
-    password: process.env.PASSWORD_BD,
-    host: process.env.HOST_BD,
-    port: process.env.PORT_BD,
-    database: process.env.DATABASE
-})
+async function initPool(){
+    try{
+        const pool = new Pool({
+            user: process.env.USERNAME_BD,
+            password: process.env.PASSWORD_BD,
+            host: process.env.HOST_BD,
+            port: process.env.PORT_BD,
+            database: process.env.DATABASE
+        });    
+        return pool;
+    }
+    catch(error){
+        pool = undefined;
+        return pool;
+    }
+}
 
 module.exports = {
-    pool:pool
+    initPool: initPool  
 }
