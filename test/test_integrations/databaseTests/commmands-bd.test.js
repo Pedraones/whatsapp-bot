946aa495ed3a0533bbf308127e55ca6c2e9cd21a
Pedro Augusto <perobr2018@gmail.com>
2026-05-15 16:38:20 -0300

    dev: feat - Implementado o teste de remoção de pedido do banco

--- test/test_integrations/databaseTests/commmands-bd.test.js
+++ test/test_integrations/databaseTests/commmands-bd.test.js
@@ -1,4 +1,5 @@
 const commands = require('../../../database/commands-bd');
+const treatmentResponse = require('../../../treatments/treatmentResponse');
 
 test("Verificação se o comando de ver todos os pedidos", async() => {
     const res = await commands.getWithoutParameters();
@@ -18,4 +19,13 @@ test("Verificação se o comando de adicionar pedido esta funcionando com '.' de
 test("Verificação se o comando de adicionar pedido esta funcionando sem '.' definido no campo valor", async() => {
     const res = await commands.createNewOrder("teste automatizado", 50, "4 broches");
     expect(res).toBe('Pedido adicionado');
+});
+
+test("Verificação se o comando de deletar pedido esta funcionando", async() => {
+    let orders = await commands.getWithoutParameters();
+
+    anyOrder = treatmentResponse.treatmentResponseBD(orders[0].row);
+
+    const res = await commands.deleteOrder(anyOrder[0]);
+    expect(res).toBe('Pedido deletado');
 });
