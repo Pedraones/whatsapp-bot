d5ad6c5c18c820db8cf94a0aca245bbe2f3a9fd8
Pedro Augusto <perobr2018@gmail.com>
2026-05-14 21:25:13 -0300

    dev: feat - Teste de verificação da requisição para a API para enviar a estrutura de ver um pedido especifico

--- test/test_integrations/apiTests/sendMessages.test.js
+++ test/test_integrations/apiTests/sendMessages.test.js
@@ -1,9 +1,15 @@
 const sendMessages = require('../../../api/sendMessages');
 
+/*
 test("Teste de requisição para enviar a lista de comando", async() => {
     expect(await sendMessages.sendListCommands()).toBeTruthy();
 });
 
 test("Teste de requisição para enviar a estrutura de adicionar um pedido", async() => {
     expect(await sendMessages.sendStructAddOrder()).toBeTruthy();
+});
+*/
+
+test("Teste de requisição para enviar a estrutura de como ver um pedido especifico", async() => {
+    expect(await sendMessages.sendStructGetOrderSpecified()).toBeTruthy();
 });
